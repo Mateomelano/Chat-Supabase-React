273c6a9b942e324aabb865398d901750330d83d8
Mateo Melano <mateomelano2003@gmail.com>
2024-09-12 15:18:08 -0300

    Trabajo Finalizado

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,27 +1,25 @@
-import { useEffect } from "react";
 import { formatDate } from "../helpers/formatDate";
+import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
 
-const Message = ({ message, date, email }) => {
-
-    formatDate(date);
+const Message = ({message, date, email}) => {
+    const [user, setUser] = useState("")
 
     const getSeccion = async () => {
         const {data} = await supabase.auth.getSession();
-        console.log(data.session.user.email);
+        setUser(data.session.user.email);
     }
-
-    useEffect(() => {
-        getSeccion();
-    }, []);
-
-    return ( 
-        <div className="card">
+    useEffect( () =>{
+        getSeccion()
+    }, [])
+    
+    return (
+        <div className={`card ${user===email?"me":""}`}>
             <p>{message}</p>
             <span>{formatDate(date)}</span>
-            <span className="user-email">{email}</span>
+            <span className="user-email">{email.split('@')[0]}</span>
         </div>
-     );
+    );
 }
  
 export default Message;
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,56 +1,53 @@
 import { supabase } from "../supabaseClient";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react'
 
 import Message from "./Message";
 import SendMessage from "./SendMessage";
 import Header from "./Header";
 
 const Messages = () => {
-  const [messages, setMessages] = useState([]);
-  const scroll = useRef();
+    const [messages, setMessages] = useState([]);
+    const scroll = useRef();
 
-  const callSupabase = async () => {
-    const { data } = await supabase.from("messages").select("*");
-    setMessages(data);
-  };
-  useEffect(() => {
-    callSupabase();
-  }, []);
+    const callSupabase = async () => {
+        const {data} = await supabase.from('messages').select('*')
+        setMessages(data);
+    }
+    useEffect( () => {
+        callSupabase()
+    }, [])
+    
+    useEffect(() => {
+        const channel = supabase
+            .channel("*")
+            .on("postgres_changes", { event: "INSERT", schema: "public", table: "messages" },
+            (payload) => {
+                const newMessage = payload.new;
+                setMessages(messages => [...messages, newMessage]);
+            })
+            .subscribe();
+            
+            return () => supabase.removeChannel(channel);
+    }, [])
 
-  useEffect(() => {
-    const channel = supabase
-      .channel("*")
-      .on(
-        "postgres_changes",
-        { event: "INSERT", schema: "public", table: "messages" },
-        (payload) => {
-          const newMessage = payload.new;
-          setMessages((messages) => [...messages, newMessage]);
-        }
-      )
-      .subscribe();
-
-    return () => supabase.removeChannel(channel);
-  }, []);
-
-  return (
-    <section className="messages">
-      <Header />
-      <div className="content">
-        {messages &&
-          messages.map((item, index) => (
-            <Message
-              key={index}
-              message={item.content}
-              date={item.created_at}
-              email={item.email}
-            />
-          ))}
-      </div>
-      <SendMessage scroll={scroll} />
-      <span ref={scroll}></span>
-    </section>
-  );
-};
-
+    return (
+        <section className='messages'>
+            <Header />
+            <div className="content">
+            {
+                messages && 
+                messages.map((item, index) => (
+                    <Message key={index} 
+                        message={item.content} 
+                        date={item.created_at} 
+                        email={item.email}/>
+                ))
+            }
+            </div>
+            <SendMessage scroll={scroll}/>
+            <span ref={scroll}></span>
+        </section>
+    );
+}
+ 
 export default Messages;
